8457bcb9b5634050aefb59c4a5f1103c1e51eba7
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2021-04-28 10:48:00 -0700

    added boiler plate code to several files - config, server, api index routes

---
+++ models/location.js
@@ -0,0 +1,10 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Location extends Model {}
+
+Location.init(
+    {
+        
+    }
+)
